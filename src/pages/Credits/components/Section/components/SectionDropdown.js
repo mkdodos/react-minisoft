238ee3d3d54af461d2c14debdd19ab4b67fcda0e
mk <mkdodos@gmail.com>
2023-06-28 09:36:47 +0800

    local

---
+++ src/pages/Credits/components/Section/components/SectionDropdown.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Dropdown } from 'semantic-ui-react';
+import { useEffect, useState } from 'react';
+import { db_money2022 as db } from '../../../../../utils/firebase';
+
+export default function SectionDropdown({ onChange }) {
+  // 編輯列
+  const [rows, setRows] = useState([]);
+  // firebase 集合
+  const dbCol = db.collection('sections');
+
+  // 取得資料
+  useEffect(() => {
+    dbCol
+      .orderBy('section', 'desc')
+      .get()
+      .then((snapshot) => {
+        let data = snapshot.docs.map((doc) => {
+          return {
+            key: doc.id,
+            text: doc.data().section,
+            value: doc.data().section,
+          };
+        });
+        setRows(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <Dropdown
+        clearable
+        onChange={(e,obj)=>onChange(e,obj)}      
+        fluid
+        selection
+        options={rows}
+      />
+    </div>
+  );
+}
--- src/pages/Credits/index.js
+++ src/pages/Credits/index.js
@@ -2,19 +2,28 @@ import React, { useEffect, useState } from 'react';
 import { Button, Input, Divider } from 'semantic-ui-react';
 import { db_money2022 as db } from '../../utils/firebase';
 import DataList from './components/DataList';
+import SectionDropdown from './components/Section/components/SectionDropdown';
 
 export default function Index() {
   // *************資料************
-  // 期數
+  // 選擇的期數
   const [section, setSection] = useState('');
-  // 本地資料
+  // 最新期數
+  const [newestSection, setNewestSection] = useState('');
+  // 本地資料(顯示用)
   const [localData, setLocalData] = useState([]);
+  // 本地資料(篩選用)
   const [localDataCopy, setLocalDataCopy] = useState([]);
+
   // *************方法************
   // 查詢
-  const handleFilter = () => {
+  const handleFilter = (e,{value}) => {
+    const section = value;
+    // console.log(section)
     // return;
     // 本地篩選
+   
+
     let rows = localDataCopy.filter((item) => item.section.includes(section));
 
     // 有查到
@@ -53,32 +62,50 @@ export default function Index() {
   // 取得遠端資料
   const fetchRemoteData = () => {
     db.collection('sections')
-      .where('section', '==', '11206')
+      .orderBy('section', 'desc')
       .get()
       .then((snapshot) => {
-        const data = snapshot.docs.map((doc) => {
-          return { ...doc.data(), id: doc.id };
-        });
-        // 將資料存入本地
-        setLocalData(data);
-        setLocalDataCopy(data);
-        console.log(data);
+        const section = snapshot.docs[0].data().section;
+        // setNewestSection(section);
+        db.collection('sections')
+          .where('section', '==', section)
+          .get()
+          .then((snapshot) => {
+            const data = snapshot.docs.map((doc) => {
+              return { ...doc.data(), id: doc.id };
+            });
+            // 將資料存入本地
+            setLocalData(data);
+            setLocalDataCopy(data);
+            console.log(data);
+          });
       });
   };
   // 設定 section
-  const handleSectionChange = (e) => {
-    // console.log(e.target.value)
-    setSection(e.target.value);
+  const handleSectionChange = (e,{value}) => {
+    console.log(value)
+    setSection(value);
   };
+  // 取得最新期數
+  // const fetchNewestSection = () => {
+  //   db.collection('sections')
+  //     .orderBy('section', 'desc')
+  //     .get()
+  //     .then((snapshot) => {
+  //       const section = snapshot.docs[0].data().section;
+  //       setNewestSection(section);
+  //     });
+  // };
   useEffect(() => {
-    // console.clear();
-
+    console.clear();
+    // fetchNewestSection();
     fetchRemoteData();
   }, []);
   return (
     <div>
-      <Input onChange={handleSectionChange} />
-      <Button onClick={handleFilter}>查詢</Button>
+      <SectionDropdown onChange={handleFilter} />
+      {/* <Input onChange={handleSectionChange} /> */}
+      {/* <Button onClick={handleFilter}>查詢</Button> */}
       <Divider />
       <DataList rows={localData} />
     </div>
