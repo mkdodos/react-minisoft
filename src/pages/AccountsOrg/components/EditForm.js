5f85c9daf361193a0257676dc8d4c4f68316d5b5
mk <mkdodos@gmail.com>
2023-07-10 18:02:26 +0800

    edit reducer

--- src/pages/AccountsOrg/components/EditForm.js
+++ src/pages/AccountsOrg/components/EditForm.js
@@ -5,7 +5,7 @@ import { Form, Button } from 'semantic-ui-react';
 export default function EditForm({ rowDispatch, row, rowsDispatch }) {
   function handleChange(e) {
     rowDispatch({
-      type: 'input-change',
+      type: 'INPUT_CHANGE',
       payload: { name: e.target.name, value: e.target.value },
     });
   }
@@ -14,10 +14,7 @@ export default function EditForm({ rowDispatch, row, rowsDispatch }) {
     e.preventDefault();
     if (row.index > -1) {
       rowsDispatch({ type: 'UPDATE_ROW', payload: { row, index: row.index } });
-      console.log('update');
-      console.log(row);
     } else {
-      rowDispatch({ type: 'submit' });
       const fetchRow = async () => {
         const [data] = await Promise.all([addRow(row)]);
         rowsDispatch({ type: 'ADD_ROW', payload: { ...row, id: data } });
@@ -32,11 +29,21 @@ export default function EditForm({ rowDispatch, row, rowsDispatch }) {
       <Form.Group>
         <Form.Field>
           <label>名稱</label>
-          <input type="text" name="name" value={row.name} onChange={handleChange} />
+          <input
+            type="text"
+            name="name"
+            value={row.name}
+            onChange={handleChange}
+          />
         </Form.Field>
         <Form.Field>
           <label>順序</label>
-          <input type="number" name="prior" value={row.prior} onChange={handleChange} />
+          <input
+            type="number"
+            name="prior"
+            value={row.prior}
+            onChange={handleChange}
+          />
         </Form.Field>
       </Form.Group>
       <Button type="submit">Submit</Button>
--- src/pages/AccountsOrg/reducer/rowReducer.js
+++ src/pages/AccountsOrg/reducer/rowReducer.js
@@ -1,17 +1,12 @@
 export default function rowReducer(row, action) {
   switch (action.type) {
-    case 'input-change':
+    case 'INPUT_CHANGE':
       return { ...row, [action.payload.name]: action.payload.value };
-    case 'submit':
-      return { name: '', prior: '' };
-
     case 'INIT_ROW':
       return { name: '', prior: '' };
-
     case 'EDIT_ROW':
       const index = action.payload.index;
       return { ...action.payload.row, index };
-
     default:
       return row;
   }
