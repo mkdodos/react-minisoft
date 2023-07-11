ef8f4d9ec9e36f96116137a2fbc96c1269c6c14f
mk <mkdodos@gmail.com>
2023-07-11 11:43:22 +0800

    cool

--- src/pages/AccountsOrg/reducer/rowsReducer.js
+++ src/pages/AccountsOrg/reducer/rowsReducer.js
@@ -1,21 +1,22 @@
 import { deleteRow, updateRow } from '../api';
 
 export default function rowsReducer(state, action) {
+  const { rows } = state;
   switch (action.type) {
     case 'INIT_ROWS':
       return { rows: action.payload };
     case 'ADD_ROW':
-      return { rows: [...state.rows, action.payload] };
-    // return [...rows, action.payload];
-    // case 'DELETE_ROW':
-    //   const id = action.payload;
-    //   deleteRow(id);
-    //   return rows.filter((row) => row.id !== action.payload);
-    // case 'UPDATE_ROW':
-    //   updateRow(action.payload.row, action.payload.row.id);
-    //   const newRows = rows.slice();
-    //   Object.assign(newRows[action.payload.index], action.payload.row);
-    //   return newRows;
+      return { rows: [...rows, action.payload] };
+
+    case 'DELETE_ROW':
+      const id = action.payload;
+      deleteRow(id);
+      return { rows: rows.filter((row) => row.id !== action.payload) };
+    case 'UPDATE_ROW':
+      updateRow(action.payload.row, action.payload.row.id);
+      const newRows = rows.slice();
+      Object.assign(newRows[action.payload.index], action.payload.row);
+      return { rows: newRows };
 
     case 'SORT_ROW':
       let sortRows = state.rows.slice();
