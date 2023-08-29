7cf6fbfbc1212d183b80a294482a86e8c7d46c51
mk <mkdodos@gmail.com>
2023-08-29 11:09:21 +0800

    ok

--- src/app/features/works/SingleWorkPage.js
+++ src/app/features/works/SingleWorkPage.js
@@ -1,12 +1,40 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchArrDone, selectArrDone } from './worksSlice';
+import { List, Table, Label } from 'semantic-ui-react';
 
 export default function SingleWorkPage() {
-  const {workId}=useParams();
+  const dispatch = useDispatch();
+  const arrdones = useSelector(selectArrDone);
+  const { workId } = useParams();
+  useEffect(() => {
+    dispatch(fetchArrDone(workId));
+  }, []);
+
   return (
-    <div>SingleWorkPage
-      {workId}
+    <div>
+      <Table unstackable>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>姓名</Table.HeaderCell>
+            <Table.HeaderCell>開始時間</Table.HeaderCell>
+            <Table.HeaderCell>完工數</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {arrdones.map((arr) => {
+            return (
+              <Table.Row key={arr.id}>
+                <Table.Cell>{arr.empName}</Table.Cell>
+                <Table.Cell>{arr.sTime}</Table.Cell>
+                <Table.Cell>{arr.doneQty}</Table.Cell>
+              </Table.Row>
+            );
+          })}
+        </Table.Body>
+      </Table>
     </div>
-  )
+  );
 }
--- src/app/features/works/worksSlice.js
+++ src/app/features/works/worksSlice.js
@@ -6,17 +6,32 @@ import axios from 'axios';
 
 const initialState = {
   data:[],
+  arrdone:[],
   status:'idle' // idle | succeeded
 };
 
+const host = 'http://192.168.0.12:8888/pdo-salary';
+
 export const fetchData = createAsyncThunk('works/fetchData', async () => {
   // const url = 'http://server2000:8888/pdo-salary/works/read.php';
-  const url = 'http://192.168.0.12:8888/pdo-salary/works/read.php';
+  const url = `${host}/works/read.php`;
   const response = await axios.get(url, {
     params: { from: '2023-01-01', to: '2023-02-02' },
   });
   return response.data;
 });
+
+
+
+export const fetchArrDone = createAsyncThunk('works/fetchArrDone', async (workId) => {
+  const url = `${host}/arrdones/read.php`;
+  const response = await axios.get(url, {
+    params: { workId },
+  });
+  return response.data;
+});
+
+
 
 const slice = createSlice({
   name: 'works',
@@ -29,9 +44,15 @@ const slice = createSlice({
       state.status = 'succeeded'
       // return action.payload;
     });
+    // 排程完工載入完成
+    builder.addCase(fetchArrDone.fulfilled, (state, action) => {
+      state.arrdone = action.payload;
+      state.status = 'succeeded'      
+    });
   },
 });
 
 export default slice.reducer;
 export const selectData = (state) => state.works.data;
+export const selectArrDone = (state) => state.works.arrdone;
 export const getStatus = (state) => state.works.status;
